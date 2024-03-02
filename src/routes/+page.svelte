77cfbeae89d82fffd5992c3250cc49ae715ea38d
dedi <80522865+ddeeddii@users.noreply.github.com>
2024-03-02 22:40:45 +0100

    feat: prototype ui layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button'
+  import * as Card from '$lib/components/ui/card'
+</script>
+
+<div class="mt-32 flex justify-center">
+  <Card.Root class="w-80">
+    <Card.Header>
+      <Card.Title class="text-center text-3xl">EzTools</Card.Title>
+      <Card.Description class="text-center">Development Preview</Card.Description>
+    </Card.Header>
+    <Card.Content class="flex justify-center">
+      <Button href="/ezitems" variant="outline">EzItems</Button>
+    </Card.Content>
+  </Card.Root>
+</div>
