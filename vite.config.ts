9b462415f45c23109f32bac03d62a846d8809cb1
dedi <80522865+ddeeddii@users.noreply.github.com>
2024-03-04 20:47:44 +0100

    style: run prettier on all files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import path from "path";
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	resolve: {
+  plugins: [sveltekit()],
+  resolve: {
     alias: {
-      $lib: path.resolve("./src/lib"),
-    },
-  },
-});
+      $lib: path.resolve('./src/lib')
+    }
+  }
+})
