d91e9c5a08d95e197088cf226f541ff607305b61
dedi <80522865+ddeeddii@users.noreply.github.com>
2024-08-16 15:52:34 +0200

    feat: fix hacky height implementation in menu (#4)

--- src/routes/ezitems/components/Menu.svelte
+++ src/routes/ezitems/components/Menu.svelte
@@ -21,20 +21,19 @@
   <Dialog.Trigger>
     <Button class="mt-2" variant="outline">Menu</Button>
   </Dialog.Trigger>
-  <Dialog.Content class="h-5/6 max-w-[90%] overflow-hidden p-6 xl:w-4/6">
-    <Dialog.Header>
-      <Dialog.Title>Menu</Dialog.Title>
-    </Dialog.Header>
-    <Tabs.Root value="config" class="h-full">
-      <Tabs.List class="mt-2 grid w-full grid-cols-3">
-        <Tabs.Trigger value="config">Config</Tabs.Trigger>
-        <Tabs.Trigger value="data">Data</Tabs.Trigger>
-        <Tabs.Trigger value="info">Information</Tabs.Trigger>
-      </Tabs.List>
-      <Tabs.Content value="config">
-        <!-- HACK still trying to figure out how to make it work without explicit heights -->
-        <ScrollArea class="h-[60vh] sm:h-[70vh] sm:pr-4">
-          <div class="flex flex-col gap-4 pb-10 sm:pb-2">
+  <Dialog.Content class="h-5/6 max-w-[90%] p-6 xl:w-4/6">
+    <Tabs.Root value="config" class="flex h-fit flex-col overflow-y-hidden">
+      <Dialog.Header>
+        <Dialog.Title>Menu</Dialog.Title>
+        <Tabs.List class="mt-2 grid w-full grid-cols-3">
+          <Tabs.Trigger value="config">Config</Tabs.Trigger>
+          <Tabs.Trigger value="data">Data</Tabs.Trigger>
+          <Tabs.Trigger value="info">Information</Tabs.Trigger>
+        </Tabs.List>
+      </Dialog.Header>
+      <ScrollArea class="h-full">
+        <Tabs.Content value="config">
+          <div class="flex flex-col gap-4 pr-3">
             <ModTemplateSetting />
             <MinifyModSetting />
             <ThresholdSetting />
@@ -45,18 +44,16 @@
               <ResetSettingsButton bind:menuState />
             </div>
           </div>
-        </ScrollArea>
-      </Tabs.Content>
-      <Tabs.Content value="data" class="h-full">
-        <ScrollArea class="h-[60vh] sm:h-[70vh]">
+        </Tabs.Content>
+
+        <Tabs.Content value="data" class="overflow-y-hidden">
           <DataPage />
-        </ScrollArea>
-      </Tabs.Content>
-      <Tabs.Content value="info">
-        <ScrollArea class="h-[60vh] sm:h-[70vh]">
+        </Tabs.Content>
+
+        <Tabs.Content value="info" class="pb-2 pr-3">
           <InfoPage />
-        </ScrollArea>
-      </Tabs.Content>
+        </Tabs.Content>
+      </ScrollArea>
     </Tabs.Root>
   </Dialog.Content>
 </Dialog.Root>
